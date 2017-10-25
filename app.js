bbb5eeb178032fc6f2c168738391ba3a21995ba1
edwin sierra <eassblack@gmil.com>
2017-10-25 10:55:45 -0500

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+mongoose.connect('mongodb://localhost:2021/3-D_matrix', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+});
 
 var router = express.Router();
 
@@ -15,6 +22,22 @@ router.get('/', function(req, res) {
 
 app.use(router);
 
+var matrixController = require('./controllers/3-D_matrix');
+
+// API routes
+var matrix = express.Router();
+
+matrix.route('/matrix')
+  .post(matrixController.createMatrix);
+
+matrix.route('/matrix/:id')
+  .put(matrixController.updateMatrix)
+  .post(matrixController.operateMatrix);
+
+app.use(matrix);
+
+
+
 app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
-});
+    console.log("Node server running on http://localhost:3000");
+  });
